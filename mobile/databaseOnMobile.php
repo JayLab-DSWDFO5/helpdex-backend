7aae6fe671f7c8e4aeedf89c9b3ba1df49f99f70
JayLab-DSWDFO5 <jmlabrador.fo5@dswd.gov.ph>
2024-10-21 11:20:30 +0800

    change db user name to dswdfo5-backend

--- mobile/databaseOnMobile.php
+++ mobile/databaseOnMobile.php
@@ -2,7 +2,7 @@
 // Cloud SQL connection details
 $host = '35.201.222.190';       // Replace with your Cloud SQL public IP address
 $db_name = 'dswd-helpdex';      // Your database name
-$username = 'dswdfo5-helpdex';  // Your database username
+$username = 'dswdfo5-backend';  // Your database username
 $password = 'DSWDfo5';           // Your database password
 
 // Directly establish the connection using procedural MySQLi
