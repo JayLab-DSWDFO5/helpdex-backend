982307a2673257461d66e8f4b536cb2732d54208
JayLab-DSWDFO5 <jmlabrador.fo5@dswd.gov.ph>
2024-10-21 14:09:55 +0800

    Update completeTask.php

--- mobile/completeTask.php
+++ mobile/completeTask.php
@@ -25,12 +25,6 @@ $status = $data['status'];
 $completion_time = $data['completion_time'];
 $resolution_notes = $data['resolution_notes'];
 
-// Validate the date format for completion_time (Y-m-d H:i:s)
-if (!preg_match('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/', $completion_time)) {
-    http_response_code(400);
-    echo json_encode(['status' => 'error', 'message' => 'Invalid completion_time format']);
-    exit;
-}
 
 // Prepare the SQL query to update the ticket status and completion details
 $query = "UPDATE requests SET `status` = ?, `completion_time` = ?, `resolution_notes` = ? WHERE `request_tracker` = ?";
