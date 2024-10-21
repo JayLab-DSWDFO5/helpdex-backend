c0bb5dc9dc00951d24b4e663da3fe9816ee8855d
JayLab-DSWDFO5 <jmlabrador.fo5@dswd.gov.ph>
2024-10-21 11:50:20 +0800

    Update databaseOnMobile.php

--- mobile/databaseOnMobile.php
+++ mobile/databaseOnMobile.php
@@ -2,11 +2,11 @@
 // Cloud SQL connection details
 $host = '35.201.222.190';       // Replace with your Cloud SQL public IP address
 $db_name = 'dswd-helpdex';      // Your database name
-$usernamedb = 'dswdfo5-backend';  // Your database username
-$passworddb = 'DSWDfo5';           // Your database password
+$username = 'dswdfo5-helpdex';  // Your database username
+$password = 'DSWDfo5';           // Your database password
 
 // Directly establish the connection using procedural MySQLi
-$conn = mysqli_connect($host, $usernamedb, $passworddb, $db_name);
+$conn = mysqli_connect($host, $username, $password, $db_name);
 
 // Check connection
 if (!$conn) {
